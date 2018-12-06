a70d9585cd61f12f0d5376dbe2bf2d77d0f8cb3d
Desmond <tws0002@gmail.com>
2018-12-06 11:41:54 +0800

    update

--- anima/env/houdini/__init__.py
+++ anima/env/houdini/__init__.py
@@ -459,6 +459,7 @@ class Houdini(EnvironmentBase):
 
 class FileHistory(object):
     """A Houdini recent file history parser
+    
     Holds the data in a dictionary, where the keys are the file types and the
     values are string list of recent file paths of that type
     """
